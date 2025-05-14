195434b3d241d4ecf2547e8a31043f61e66b0fc5
Bruno <ask@bruno.wtf>
2025-05-14 10:01:00 +0000

    vega_util

---
+++ README.md
@@ -0,0 +1,11 @@
+
+Start the server in the repo root directory so the `./rewards/` folder is in scope.
+
+This limits the Cache to 500MB:
+
+```bash
+$ ./vegafusion-server --host 127.0.0.1 --port 50051 --web --memory-limit 524288000
+ ```
+
+
+Also copy vega_utils to `/frontend-minimal/node_modules/vegafusion-wasm/snippets/vegafusion-wasm-f7e76dd0896f3e00/js/vega_utils.js` for webpack to compile.
---
+++ vega_utils.js
@@ -0,0 +1,135 @@
+import { truthy } from "vega-util"
+import * as grpcWeb from 'grpc-web';
+
+import _ from "lodash"
+
+export function localTimezone() {
+    return Intl.DateTimeFormat().resolvedOptions().timeZone
+}
+
+// JSON Serialize Dates to milliseconds
+Object.defineProperty(Date.prototype, "toJSON", {value: function() {return this.getTime()}})
+
+function getNestedRuntime(view, scope) {
+    // name is an array that may have leading integer group indices
+    var runtime = view._runtime;
+    for (const index of scope) {
+        runtime = runtime.subcontext[index];
+    }
+    return runtime
+}
+
+function lookupSignalOp(view, name, scope) {
+    // name is an array that may have leading integer group indices
+    let parent_runtime = getNestedRuntime(view, scope);
+    return parent_runtime.signals[name] ?? null;
+}
+
+function dataref(view, name, scope) {
+    // name is an array that may have leading integer group indices
+    let parent_runtime = getNestedRuntime(view, scope);
+    return parent_runtime.data[name];
+}
+
+export function getSignalValue(view, name, scope) {
+    let signal_op = lookupSignalOp(view, name, scope);
+    return _.cloneDeep(signal_op.value)
+}
+
+export function setSignalValue(view, name, scope, value) {
+    let signal_op = lookupSignalOp(view, name, scope);
+    view.update(signal_op, value);
+}
+
+export function getDataValue(view, name, scope) {
+    let data_op = dataref(view, name, scope);
+    return _.cloneDeep(data_op.values.value)
+}
+
+export function setDataValue(view, name, scope, value) {
+    let dataset = dataref(view, name, scope);
+    let changeset = view.changeset().remove(truthy).insert(value)
+    dataset.modified = true;
+    view.pulse(dataset.input, changeset);
+}
+
+export function addSignalListener(view, name, scope, handler, wait, maxWait) {
+    let signal_op = lookupSignalOp(view, name, scope);
+    let options = {};
+    if (maxWait) {
+        options["maxWait"] = maxWait;
+    }
+
+    return addOperatorListener(
+        view,
+        name,
+        signal_op,
+        _.debounce(handler, wait, options),
+    );
+}
+
+export function addDataListener(view, name, scope, handler, wait, maxWait) {
+    let dataset = dataref(view, name, scope).values;
+    let options = {};
+    if (maxWait) {
+        options["maxWait"] = maxWait;
+    }
+    return addOperatorListener(
+        view,
+        name,
+        dataset,
+        _.debounce(handler, wait, options),
+    );
+}
+
+// Private helpers from Vega
+function findOperatorHandler(op, handler) {
+    const h = (op._targets || [])
+        .filter(op => op._update && op._update.handler === handler);
+    return h.length ? h[0] : null;
+}
+
+function addOperatorListener(view, name, op, handler) {
+    let h = findOperatorHandler(op, handler);
+    if (!h) {
+        h = trap(view, () => handler(name, op.value));
+        h.handler = handler;
+        view.on(op, null, h);
+    }
+    return view;
+}
+
+function trap(view, fn) {
+    return !fn ? null : function() {
+        try {
+            fn.apply(this, arguments);
+        } catch (error) {
+            view.error(error);
+        }
+    };
+}
+
+// Other utility functions
+export function make_grpc_send_message_fn(client, hostname) {
+    let send_message_grpc = async (send_msg_bytes) => {
+        let grpc_route = '/services.VegaFusionRuntime/TaskGraphQuery'
+
+        // Make custom MethodDescriptor with all required properties
+        const methodDescriptor = new grpcWeb.MethodDescriptor(
+            grpc_route,
+            grpcWeb.MethodType.UNARY,
+            Uint8Array,
+            Uint8Array,
+            (req) => req,
+            (res) => res,
+        );
+
+        return await client.thenableCall(
+            hostname + grpc_route,
+            send_msg_bytes,
+            {},
+            methodDescriptor,
+        )
+    }
+    return send_message_grpc
+}
